2eb9aeb25fb1b290772414f93e2bcf13dd96b829
David Barrat <david@barrat.io>
2024-11-10 19:32:42 +0100

    chore: init repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module koyeb/naas
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello")
+}
+
+// import (
+// 	"github.com/dbrrt/yart/routing"
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func main() {
+// 	r := setupServer()
+// 	r.Run(":8080")
+// }
+
+// func setupServer() *gin.Engine {
+// 	return routing.RomanRouter()
+// }

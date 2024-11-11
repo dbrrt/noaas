56c1c3ee7d3143cd9bfae1e470351a266f855d10
David Barrat <david@barrat.io>
2024-11-11 14:05:25 +0100

    linting, cleanup test

--- main_test.go
+++ main_test.go
@@ -19,7 +19,7 @@ func TestHealthRoute(t *testing.T) {
 	ts := httptest.NewServer(routing.SetupServer())
 	defer ts.Close()
 
-	resp, err := http.Get(fmt.Sprintf("%s", ts.URL))
+	resp, err := http.Get(fmt.Sprintf(ts.URL))
 
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
@@ -32,32 +32,3 @@ func TestHealthRoute(t *testing.T) {
 	assert.Equal(t, ok, true)
 	assert.Equal(t, val, []string([]string{"text/plain; charset=utf-8"}))
 }
-
-// func TestIntRangeToRomanConversionValid(t *testing.T) {
-
-// 	ts := httptest.NewServer(setupServer())
-// 	defer ts.Close()
-
-// 	testRequestInput := `{"start_range": 1, "end_range": 25}`
-
-// 	resp, err := http.Post(
-// 		fmt.Sprintf("%s/v1/int/range/convert", ts.URL),
-// 		"application/json", bytes.NewBuffer([]byte(testRequestInput)))
-
-// 	assert.Equal(t, err, nil)
-
-// 	defer resp.Body.Close()
-// 	requestResponse1, err := ioutil.ReadAll(resp.Body)
-
-// 	assert.Equal(t, err, nil)
-
-// 	var responseObj ResponseStruct
-// 	if err := json.Unmarshal(requestResponse1, &responseObj); err != nil {
-// 		panic(err)
-// 	}
-
-// 	assert.Equal(t, len(responseObj.Payload), 25)
-// 	assert.Equal(t, responseObj.Payload[0], "I")
-// 	assert.Equal(t, len(responseObj.Error), 0)
-
-// }

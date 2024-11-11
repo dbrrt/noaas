f71238444c4e28d9bee0a83f1ac4d48f8cbed43d
David Barrat <david@barrat.io>
2024-11-11 13:38:49 +0100

    add readuri utilities and scaffold ci github action

--- README.md
+++ README.md
@@ -33,3 +33,5 @@ make dev
 ## Comments
 
 - busybox has been used instead of nginx, as it's simpler to configure, can receive dynamic port through command line, doesn't require a custom configuration file.
+- only one package has been defined for the sake of simplicity, in a larger codebase, I'd split across multiple package to foster better isolation
+- remote code execution is dangerous, I've skipped the sanity checks, but that's something required in a secured context.
---
+++ readuri.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os/exec"
+)
+
+func readRemoteUriPayload(uri string, script bool) (string, error) {
+	resp, err := http.Get(uri)
+	if err != nil {
+		return "", fmt.Errorf("error fetching URL: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to retrieve content. Status code: %v", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %v", err)
+	}
+
+	output := ""
+
+	if script {
+		// Run the content as a shell script
+		cmd := exec.Command("/bin/sh", "-c", string(body))
+		combinedOutput, err := cmd.CombinedOutput()
+		if err != nil {
+			return "", fmt.Errorf("error executing script: %v", err)
+		}
+		output = string(combinedOutput)
+	} else {
+		output = string(body)
+	}
+
+	return output, nil
+}
